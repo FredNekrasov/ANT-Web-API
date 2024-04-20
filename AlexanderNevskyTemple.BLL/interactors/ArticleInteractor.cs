62d5ba02943191893e96fcd093d591cd2e701985
FredNekrasov <frednekrasov49@gmail.com>
2024-04-20 18:59:37 +0300

    creating interactors

---
+++ AlexanderNevskyTemple.BLL/interactors/ArticleInteractor.cs
@@ -0,0 +1,25 @@
+﻿using AlexanderNevskyTemple.DAL.entities;
+using AlexanderNevskyTemple.DAL.repositories;
+
+namespace AlexanderNevskyTemple.BLL.interactors;
+public class ArticleInteractor(ArticleRepository repository) {
+    private readonly ArticleRepository _repository = repository;
+    public async Task<bool> DeleteAsync(Guid id) {
+        var list = await _repository.GetListAsync();
+        var entity = list.FirstOrDefault(i => i.Id == id);
+        if(entity == null) return false;
+        await _repository.DeleteAsync(entity);
+        return true;
+    }
+    public async Task<List<Article>> GetListAsync() => await _repository.GetListAsync();
+    public async Task<bool> InsertAsync(Article? entity) {
+        if(entity == null) return false;
+        await _repository.InsertAsync(entity);
+        return true;
+    }
+    public async Task<bool?> UpdateAsync(Guid id, Article entity) {
+        if (entity.Id != id) return false;
+        if (!await _repository.EntityExistsAsync(id)) return null;
+        return await _repository.UpdateAsync(entity);
+    }
+}
---
+++ AlexanderNevskyTemple.BLL/interactors/CatalogInteractor.cs
@@ -0,0 +1,25 @@
+﻿using AlexanderNevskyTemple.DAL.entities;
+using AlexanderNevskyTemple.DAL.repositories;
+
+namespace AlexanderNevskyTemple.BLL.interactors;
+public class CatalogInteractor(CatalogRepository repository) {
+    private readonly CatalogRepository _repository = repository;
+    public async Task<bool> DeleteAsync(int id) {
+        var list = await _repository.GetListAsync();
+        var entity = list.FirstOrDefault(i => i.Id == id);
+        if(entity == null) return false;
+        await _repository.DeleteAsync(entity);
+        return true;
+    }
+    public async Task<List<Catalog>> GetListAsync() => await _repository.GetListAsync();
+    public async Task<bool> InsertAsync(Catalog? entity) {
+        if(entity == null) return false;
+        await _repository.InsertAsync(entity);
+        return true;
+    }
+    public async Task<bool?> UpdateAsync(int id, Catalog entity) {
+        if (entity.Id != id) return false;
+        if (!await _repository.EntityExistsAsync(id)) return null;
+        return await _repository.UpdateAsync(entity);
+    }
+}
