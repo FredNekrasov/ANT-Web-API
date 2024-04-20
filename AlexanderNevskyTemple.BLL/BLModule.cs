72259f7cf9a38c7513daeb1ce47e5b1563a395d8
FredNekrasov <frednekrasov49@gmail.com>
2024-04-20 17:26:16 +0300

    creating an extension functions for IServiceCollection

--- AlexanderNevskyTemple.BLL/BLModule.cs
+++ AlexanderNevskyTemple.BLL/BLModule.cs
@@ -1,6 +1,11 @@
-﻿namespace AlexanderNevskyTemple.BLL;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AlexanderNevskyTemple.BLL;
 
 public static class BLModule
 {
-
+    public static IServiceCollection AddBLModule(this IServiceCollection services)
+    {
+        return services;
+    }
 }
--- AlexanderNevskyTemple.DAL/DALModule.cs
+++ AlexanderNevskyTemple.DAL/DALModule.cs
@@ -1,6 +1,13 @@
-﻿namespace AlexanderNevskyTemple.DAL;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AlexanderNevskyTemple.DAL;
 
 public static class DALModule
 {
-
+    public static IServiceCollection AddDALModule(this IServiceCollection services, string? connectionString)
+    {
+        services.AddDbContext<ANTDbContext>(options => options.UseSqlServer(connectionString));
+        return services;
+    }
 }
