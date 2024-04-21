7c547d02de21bb9735e0b12aee4c840a430bb8d2
FredNekrasov <frednekrasov49@gmail.com>
2024-04-21 13:40:51 +0300

    changing the catalog data type in the ArticleDto class

--- AlexanderNevskyTemple.WebAPI/dto/ArticleDto.cs
+++ AlexanderNevskyTemple.WebAPI/dto/ArticleDto.cs
@@ -1,9 +1,7 @@
-﻿using AlexanderNevskyTemple.DAL.entities;
-
-namespace AlexanderNevskyTemple.WebAPI.dto; 
+﻿namespace AlexanderNevskyTemple.WebAPI.dto;
 public class ArticleDto {
     public long Id { get; set; }
-    public Catalog Catalog { get; set; }
+    public CatalogDto Catalog { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Date { get; set; } = string.Empty;
