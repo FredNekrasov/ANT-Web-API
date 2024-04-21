ded5ad432fd341e857aac344c2e064d8741b8dec
FredNekrasov <frednekrasov49@gmail.com>
2024-04-21 13:44:48 +0300

    creating an extension function to convert from entity to dto and vice versa

---
+++ AlexanderNevskyTemple.WebAPI/mappers/CatalogMapper.cs
@@ -0,0 +1,14 @@
+﻿using AlexanderNevskyTemple.DAL.entities;
+using AlexanderNevskyTemple.WebAPI.dto;
+
+namespace AlexanderNevskyTemple.WebAPI.mappers; 
+public static class CatalogMapper {
+    public static CatalogDto ToDto(this Catalog entity) => new() {
+        Id = entity.Id,
+        Name = entity.Name
+    };
+    public static Catalog ToEntity(this CatalogDto dto) => new() {
+        Id = dto.Id,
+        Name = dto.Name
+    };
+}
