d73d439da841091250fb2eebf7b9181de1d22f04
FredNekrasov <frednekrasov49@gmail.com>
2024-05-08 19:33:19 +0300

    creating the interactor interface for working with DI

---
+++ AlexanderNevskyTemple.BLL/interactors/IInteractor.cs
@@ -0,0 +1,7 @@
+﻿namespace AlexanderNevskyTemple.BLL.interactors;
+public interface IInteractor<M, E> {
+    Task<bool?> DeleteAsync(E id);
+    Task<List<M>> GetListAsync();
+    Task<bool> InsertAsync(M? entity);
+    Task<bool?> UpdateAsync(E id, M entity);
+}
