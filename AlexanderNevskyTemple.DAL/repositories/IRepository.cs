7a058235041dbf55353b7aeb199e77a756665b02
FredNekrasov <frednekrasov49@gmail.com>
2024-05-08 19:28:56 +0300

    creating the repository interface for working with DI

---
+++ AlexanderNevskyTemple.DAL/repositories/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace AlexanderNevskyTemple.DAL.repositories;
+public interface IRepository<E, I> {
+    Task InsertAsync(E entity);
+    Task<bool> DeleteAsync(E entity);
+    Task<List<E>> GetListAsync();
+    Task<bool> UpdateAsync(E entity);
+    Task<bool> EntityExistsAsync(I id);
+}
