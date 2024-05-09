2096463c5f29ba0c79d1e3d9e44624a603b4b5b9
FredNekrasov <frednekrasov49@gmail.com>
2024-05-09 18:05:43 +0300

    transferring the repository interface from DAL to BLL

---
+++ AlexanderNevskyTemple.BLL/repository/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace AlexanderNevskyTemple.BLL.repository;
+public interface IRepository<M, I> {
+    Task InsertAsync(M model);
+    Task<bool> DeleteAsync(M model);
+    Task<List<M>> GetListAsync();
+    Task<bool> UpdateAsync(M model);
+    Task<bool> EntityExistsAsync(I id);
+}
--- AlexanderNevskyTemple.DAL/repositories/IRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AlexanderNevskyTemple.DAL.repositories;
-public interface IRepository<E, I> {
-    Task InsertAsync(E entity);
-    Task<bool> DeleteAsync(E entity);
-    Task<List<E>> GetListAsync();
-    Task<bool> UpdateAsync(E entity);
-    Task<bool> EntityExistsAsync(I id);
-}
