3f900171a8f31e63a44ac8748134cb0ee97b213b
FredNekrasov <frednekrasov49@gmail.com>
2024-05-09 18:29:37 +0300

    refactoring the Verifications.cs class

--- AlexanderNevskyTemple.BLL/Verifications.cs
+++ AlexanderNevskyTemple.BLL/Verifications.cs
@@ -1,13 +1,13 @@
-﻿using AlexanderNevskyTemple.DAL.entities;
+﻿using AlexanderNevskyTemple.BLL.models;
 
 namespace AlexanderNevskyTemple.BLL;
 public static class Verifications {//Only extenstion functions that check the data
     //simplification of conditions
     public static bool IsBlank(this string str) => string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
-    public static bool IsNegative(this long l) => l <= 0;
-    public static bool IsNegative(this int i) => i <= 0;
+    public static bool IsNegative(this long l) => l < 0;
+    public static bool IsNegative(this int i) => i < 0;
     //Validate data
-    public static bool IsDataValid(this Catalog catalog) => !(catalog.Id.IsNegative() || catalog.Name.IsBlank());
-    public static bool IsDataValid(this Article article) => !(article.Id.IsNegative() || article.Title.IsBlank() || article.Description.IsBlank() || article.CatalogId.IsNegative());
-    public static bool IsDataValid(this Content content) => !(content.Id.IsNegative() || content.Data.IsBlank() || content.ArticleId.IsNegative());
+    public static bool IsDataValid(this CatalogModel catalog) => !(catalog.Id.IsNegative() || catalog.Name.IsBlank());
+    public static bool IsDataValid(this ArticleModel article) => !(article.Id.IsNegative() || article.Title.IsBlank() || article.Catalog.Id.IsNegative());
+    public static bool IsDataValid(this ContentModel content) => !(content.Id.IsNegative() || content.Data.IsBlank() || content.ArticleId.IsNegative());
 }
