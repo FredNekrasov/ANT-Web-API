1d55ec645b1d9e1026202532bdb5f5f4dbe5c966
FredNekrasov <frednekrasov49@gmail.com>
2024-05-09 18:09:39 +0300

    creating converters from an entity to a model and vice versa

---
+++ AlexanderNevskyTemple.DAL/mappers/ArticleMapper.cs
@@ -0,0 +1,13 @@
+﻿using AlexanderNevskyTemple.BLL.models;
+using AlexanderNevskyTemple.DAL.entities;
+
+namespace AlexanderNevskyTemple.DAL.mappers; 
+public static class ArticleMapper {
+    public static Article ToEntity(this ArticleModel model) => new() {
+        Id = model.Id,
+        CatalogId = model.Catalog.Id,
+        Title = model.Title,
+        Description = model.Description,
+        DateOrBanner = model.DateOrBanner
+    };
+}
---
+++ AlexanderNevskyTemple.DAL/mappers/EntityMapper.cs
@@ -0,0 +1,15 @@
+﻿using AlexanderNevskyTemple.BLL.models;
+using AlexanderNevskyTemple.DAL.entities;
+using AutoMapper;
+
+namespace AlexanderNevskyTemple.DAL.mappers;
+public class EntityMapper : Profile {
+    public EntityMapper()
+    {
+        CreateMap<CatalogModel, Catalog>();
+        CreateMap<Catalog, CatalogModel>();
+
+        CreateMap<ContentModel, Content>();
+        CreateMap<Content, ContentModel>();
+    }
+}
