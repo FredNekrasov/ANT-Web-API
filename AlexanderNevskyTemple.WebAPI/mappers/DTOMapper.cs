dceff8131c1530a27659b55197c6019cd5e53412
FredNekrasov <frednekrasov49@gmail.com>
2024-05-09 18:19:43 +0300

    removing unnecessary mappers from the app layer and creating the DTOMapper to convert a DTO to a model and vice versa

---
+++ AlexanderNevskyTemple.WebAPI/mappers/DTOMapper.cs
@@ -0,0 +1,17 @@
+﻿using AlexanderNevskyTemple.BLL.models;
+using AlexanderNevskyTemple.WebAPI.dto;
+using AutoMapper;
+
+namespace AlexanderNevskyTemple.WebAPI.mappers;
+public class DTOMapper : Profile {
+    public DTOMapper() {
+        CreateMap<CatalogModel, CatalogDto>();
+        CreateMap<CatalogDto, CatalogModel>();
+
+        CreateMap<ArticleModel, ArticleDto>();
+        CreateMap<ArticleDto, ArticleModel>();
+
+        CreateMap<ContentModel, ContentDto>();
+        CreateMap<ContentDto, ContentModel>();
+    }
+}
