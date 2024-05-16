6f13536b5946340600e174c620717d8db8a4ca21
FredNekrasov <frednekrasov49@gmail.com>
2024-05-16 21:08:48 +0300

    creating the ChapterDto for the ChapterModel

---
+++ AlexanderNevskyTemple.WebAPI/dto/ChapterDto.cs
@@ -0,0 +1,11 @@
+﻿using AlexanderNevskyTemple.BLL.models;
+
+namespace AlexanderNevskyTemple.WebAPI.dto; 
+public class ChapterDto {
+    public long Id { get; set; }
+    public CatalogModel Catalog { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string DateOrBanner { get; set; } = string.Empty;
+    public List<string> Content { get; set; }
+}
