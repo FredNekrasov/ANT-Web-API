b6c1b101551241a02b93acf7d087b2483ea22627
FredNekrasov <frednekrasov49@gmail.com>
2024-05-16 20:59:16 +0300

    creating a new model to get the full article with links

---
+++ AlexanderNevskyTemple.BLL/models/ChapterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AlexanderNevskyTemple.BLL.models; 
+public class ChapterModel {
+    public long Id { get; set; }
+    public CatalogModel Catalog { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string DateOrBanner { get; set; } = string.Empty;
+    public List<string> Content { get; set; }
+}
